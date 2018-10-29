3df94425d544e9d3c63ceec13e0eee1e0cc66e33
Chris Russell <christine.russell10@gmail.com>
2018-10-29 13:53:41 -0400

    Updates

---
+++ precache-manifest.feefb2a310d95ba8bbdcb539964d701c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f880cf94ef251dbec505",
+    "url": "/MemoryGame/static/js/runtime~main.f880cf94.js"
+  },
+  {
+    "revision": "e87b003628e6e8ce0256",
+    "url": "/MemoryGame/static/js/main.e87b0036.chunk.js"
+  },
+  {
+    "revision": "d7c3259394c4b01131ba",
+    "url": "/MemoryGame/static/js/1.d7c32593.chunk.js"
+  },
+  {
+    "revision": "e87b003628e6e8ce0256",
+    "url": "/MemoryGame/static/css/main.90008c09.chunk.css"
+  },
+  {
+    "revision": "5fc37543dd3ac701fb94ea6503e7e999",
+    "url": "/MemoryGame/index.html"
+  }
+];
